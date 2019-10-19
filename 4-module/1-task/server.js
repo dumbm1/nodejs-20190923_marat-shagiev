1e55c664163276e05c57095ac3dc2f3832c17e6f
Marat Shagiev <m_js@bk.ru>
2019-10-19 13:00:06 +0300

    Begin 4 module 1 task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,23 +1,51 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
-
+const fs = require('fs');
 const server = new http.Server();
 
-server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
+server.on('request', async (req, res) => {
 
-  const filepath = path.join(__dirname, 'files', pathname);
+    const pathname = url.parse(req.url).pathname.slice(1);
+    const filepath = path.join(__dirname, 'files', pathname);
 
-  switch (req.method) {
-    case 'GET':
+    switch (req.method) {
+        case 'GET':
+            try {
+                if (pathname.includes('/')) {
+                    res.statusCode = 400;
+                    res.end();
+                    throw new Error('Вложенные пути не поддерживаются');
+                }
+                if (!fs.existsSync(filepath)) {
+                    res.statusCode = 404;
+                    res.end();
+                    throw new Error('Файл не найден');
+                }
 
-      break;
+                /* res.statusCode = 200;
+                 fs.createReadStream(filepath).pipe(res);*/
 
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
-  }
+                const body = [];
+                const content = fs.createReadStream(filepath);
+                for await (const chunk of content) {
+                    body.push(chunk.toString());
+                }
+
+                res.end(body);
+
+            } catch (e) {
+                console.log(e);
+            }
+            res.statusCode = 200;
+            break;
+
+        default:
+            res.statusCode = 500;
+            res.end('Not implemented');
+            break;
+    }
 });
+
 
 module.exports = server;
