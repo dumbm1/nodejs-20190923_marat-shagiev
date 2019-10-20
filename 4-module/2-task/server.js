a1086d7afc0194699910bde6206aa958b9560e91
Marat Shagiev <m_js@bk.ru>
2019-10-20 20:48:55 +0300

    Begin 4 module 2 task (1 and 3 кresolved)

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,19 +1,43 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
+const limitSizeStream = new LimitSizeStream({limit: 1000});
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'POST':
 
-      break;
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end('Вложенные папки не поддерживаются');
+      }
+      if (fs.existsSync(filepath)) {
+        res.statusCode = 409;
+        res.end('File Exists');
+      }
+      const outStream = fs.createWriteStream(filepath);
+      limitSizeStream.pipe(outStream);
+      outStream
+        .on('error', err => {
+          if(err.code === 'LIMIT_EXCEEDED') {
+            res.statusCode = 413;
+            res.end('Limit Exceeded Error');
+          } else {
+            res.statusCode = 500;
+            res.end('Internal Server Error');
+          }
 
+        })
+        .pipe(res);
+
+      break;
     default:
       res.statusCode = 501;
       res.end('Not implemented');
