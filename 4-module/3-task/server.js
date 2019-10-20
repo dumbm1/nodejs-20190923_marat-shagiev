7f29558772f6e953801261a5ff3db3484a6662df
Marat Shagiev <m_js@bk.ru>
2019-10-20 18:02:23 +0300

    Solutin to 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -6,65 +6,52 @@ const fs = require('fs');
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-    const pathname = url.parse(req.url).pathname.slice(1);
-    const filepath = path.join(__dirname, 'files', pathname);
+  const pathname = url.parse(req.url).pathname.slice(1);
+  const filepath = path.join(__dirname, 'files', pathname);
 
-    switch (req.method) {
-        case 'DELETE':
+  switch (req.method) {
+    case 'DELETE':
 
-            //region Description
+      //region Description
 
-            try {
-                if (pathname.includes('/')) {
-                    res.statusCode = 400;
-                    res.end('Bad Request');
-                    throw new Error('Вложенные папки не поддерживаются');
-                }
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end('Вложенная структура папок не поддерживается');
+      }
 
-                if (!fs.existsSync(filepath)) {
-                    res.statusCode = 404;
-                    res.end('Not Found');
-                    throw new Error('Файл не найден');
-                }
+      fs.unlink(filepath, err => {
+        if (err) {
+          if (err.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.end('Not Found');
+          } else {
+            res.statusCode = 500;
+            res.end('Internal Server Error');
+          }
+        }
+        res.statusCode = 200;
+        res.end('OK');
 
-                /*fs.unlink(filepath, (err) => {
-                    if (err) throw err;
-                    res.statusCode = 200;
-                    console.log('Файл удален!');
-                    res.end('OK');
-                });*/
-                fs.unlinkSync(filepath);
-                res.statusCode = 200;
-                console.log('Файл удален!');
-                res.end('OK');
-            } catch (e) {
-                console.log(e.message);
-            }
+      });
 
-            //endregion
-            break;
+      //endregion
+      break;
 
-        case 'PUT': // helper case - create the file
-            try {
-                if (!fs.existsSync(filepath)) {
-                    fs.writeFileSync(filepath, 'Hell node.js', 'utf-8');
-                    res.statusCode = 666;
-                    res.end('File Created');
-                    console.log('Файл создана');
-                } else {
-                    res.statusCode = 777;
-                    res.end('File Exists');
-                    throw new Error('Файл уже есть');
-                }
-            } catch (e) {
-                console.log(e.message);
-            }
-            break; // helper case - create the file
+    case 'PUT': // helper case - create file
+      fs.writeFile(filepath, 'Hell, NodeJs!', {enconding: 'utf-8'}, err => {
+        if (err) {
+          res.statusCode = 500;
+          res.end('Internal Server Error');
+        }
+        res.statusCode = 666;
+        res.end('File created');
+      });// helper case - create file
 
-        default:
-            res.statusCode = 501;
-            res.end('Not implemented');
-    }
+      break;
+    default:
+      res.statusCode = 501;
+      res.end('Not implemented');
+  }
 });
 
 module.exports = server;
