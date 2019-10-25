ec5293ab063ef86a07eca1ad085168ce227d1186
Marat Shagiev <m_js@bk.ru>
2019-10-25 18:11:41 +0300

    4-module 2-task try resolve via Promise.all

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -4,7 +4,9 @@ const path = require('path');
 const fs = require('fs');
 const LimitSizeStream = require('./LimitSizeStream');
 
-const limitedStream = new LimitSizeStream({limit: Math.pow(2, 20)}); // байт
+const fileSizeLimit = Math.pow(2, 2);
+
+const limitedStream = new LimitSizeStream({limit: fileSizeLimit}); // байт
 const server = new http.Server();
 
 server.on('request', (req, res) => {
@@ -12,65 +14,54 @@ server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
   const filepath = path.join(__dirname, 'files', pathname);
 
-  if (pathname.includes('/')) {
-    res.statusCode = 400;
-    res.end('Вложенные папки не поддерживаются');
-    console.log('Вложенные папки не поддерживаются');
-    return;
-  }
-
   switch (req.method) {
     case 'POST':
 
-      if (fs.existsSync(filepath)) {
-        res.statusCode = 409;
-        res.end('File Exists');
-        console.log('File Exists');
-        break;
-      }
-
-      req.pipe(limitedStream);
-
-      limitedStream.on('error', err => {
-
-        if (err.code === 'LIMIT_EXCEEDED') {
-
-          fs.unlink(filepath, err => {
-            if (err) cosole.log(err.code);
-            console.log('Файл Зачищен');
+      Promise.all([
+        new Promise((resolve, reject) => {
+          if (pathname.includes('/')) {
+            res.statusCode = 400;
+            return reject(new Error('Вложенные папки не поддерживаются'));
+          } else { return resolve('Порядок - вложенных папок нет');}
+        }),
+        new Promise((resolve, reject) => {
+          if (fs.existsSync(filepath)) {
+            res.statusCode = 409;
+            return reject(new Error('Файл существует'));
+          } else { return resolve('Порядок - файла с таким именем нет');}
+        }),
+        new Promise((resolve, reject) => {
+          req.on('data', chunk => {
+            try {
+              limitedStream.write(chunk);
+            } catch (err) {
+              if (err.code === 'LIMIT_EXCEEDED') {
+                res.statusCode = 413;
+                return reject(new Error(`Ошибка - файл > ${fileSizeLimit} байт`));
+              } else {
+                res.statusCode = 500;
+                return reject(new Error(`Неизвестная ошибка сервера`));
+              }
+            }
           });
-
-          res.statusCode = 413;
-          res.end(err.code);
-          console.log(err.code);
-        } else {
-          res.statusCode = 500;
-          res.end('Internal Server Error');
-        }
-
-      });
-
-      const file = fs.createWriteStream(filepath/*, {flags: 'wx'}*/);
-
-      req.on('end', err => {
-        if (err) {
-          console.log(err.code);
-        }
-        limitedStream.pipe(file);
-
-        console.log('Req End');
-        res.end('Req End');
-      });
-
-      file.on('close', () => {
-        console.log('File close');
-      });
-
-      req.on('close', err => {
-        if (err) {
-          console.log(err.code);
-        }
-      });
+          req.on('end', () => {
+            return resolve(`Порядок - файл < ${fileSizeLimit} байт`);
+          });
+        })
+      ])
+             .then(results => {
+               results.forEach(result => {
+                 console.log(result);
+                 console.log(res.statusCode);
+               });
+             })
+             .catch(err => {
+               console.log(err.message);
+               console.log(res.statusCode);
+             })
+             .finally(() => {
+               res.end('Конец запроса');
+             });
 
       break;
 
