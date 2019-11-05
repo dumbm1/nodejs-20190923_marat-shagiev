8ecc48af32d6c7d0a7a3dba46c05a4259f454829
Marat Shagiev <account2015@bk.ru>
2019-11-05 16:45:40 +0300

    fix server.js

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,62 +1,38 @@
-const fs = require('fs');
-const LimitSizeStream = require('./LimitSizeStream');
+const url = require('url');
+const http = require('http');
+const path = require('path');
 
-module.exports = function receiveFile(filepath, req, res) {
-  if (req.headers['content-length'] > 1e6) {
-    res.statusCode = 413;
-    res.end('File is too big!');
+const receiveFile = require('./receiveFile');
+
+const server = new http.Server();
+
+server.on('request', (req, res) => {
+  const pathname = url.parse(req.url).pathname.slice(1);
+
+  if (pathname.includes('/') || pathname.includes('..')) {
+    res.statusCode = 400;
+    res.end('Nested paths are not allowed');
     return;
   }
 
-  const writeStream = fs.createWriteStream(filepath, {flags: 'wx'});
-  const limitStream = new LimitSizeStream({limit: 1e6});
+  const filepath = path.join(__dirname, 'files', pathname);
 
-  req
-      .pipe(limitStream)
-      .pipe(writeStream);
+  switch (req.method) {
+    case 'POST':
+      if (!filepath) {
+        res.statusCode = 404;
+        res.end('File not found');
+        return;
+      }
 
-  limitStream.on('error', (err) => {
-    if (err.code === 'LIMIT_EXCEEDED') {
-      res.statusCode = 413;
-      res.setHeader('Connection', 'close');
-      res.end('File is too big');
+      receiveFile(filepath, req, res);
 
-      fs.unlink(filepath, (err) => {});
-      return;
-    }
+      break;
 
-    console.error(err);
+    default:
+      res.statusCode = 501;
+      res.end('Not implemented');
+  }
+});
 
-    res.statusCode = 500;
-    res.setHeader('Connection', 'close');
-    res.end('Internal server error');
-
-    fs.unlink(filepath, (err) => {});
-  });
-
-  writeStream
-      .on('error', (err) => {
-        if (err.code === 'EEXIST') {
-          res.statusCode = 409;
-          res.end('File exists');
-          return;
-        }
-
-        console.error(err);
-
-        res.statusCode = 500;
-        res.setHeader('Connection', 'close');
-        res.end('Internal server error');
-
-        fs.unlink(filepath, (err) => {});
-      })
-      .on('close', () => {
-        res.statusCode = 201;
-        res.end('File created');
-      });
-
-  res.on('close', () => {
-    if (res.finished) return;
-    fs.unlink(filepath, (err) => {});
-  });
-};
+module.exports = server;
