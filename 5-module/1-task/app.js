e6e8a07ca902b59c886173eca8c5cc12b0ca0df9
Marat Shagiev <m_js@bk.ru>
2019-11-08 18:51:30 +0300

    add work resolve to 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -11,24 +11,71 @@ let subscribers = [];
 
 router.get('/subscribe', async (ctx, next) => {
 
-  const promise = new Promise((resolve, reject) => subscribers.push(resolve));
+  const promise = new Promise((resolve, reject) => {
+    subscribers.push(resolve);
 
-  let message = await promise;
+    ctx.res.on("close", function () {
+      subscribers.splice(subscribers.indexOf(resolve), 1);
+      const error = new Error("Connection closed");
+      error.code = "ECONNRESET";
+      reject(error);
+    });
+
+  });
+
+  let message;
+
+  try {
+    message = await promise;
+  } catch (err) {
+    if (err.code === "ECONNRESET") return;
+    throw err;
+  }
 
   ctx.body = message;
 
 });
 
-router.post('/publish', async (ctx, next) => {
-
+router.post("/publish", async (ctx, next) => {
   const message = ctx.request.body.message;
 
-  if (!message) ctx.throw(400);
+  if (!message) {
+    ctx.throw(400);
+  }
 
-  subscribers.forEach(resolve => {resolve(message.toString())});
+  subscribers.forEach(function (resolve) {
+    resolve(String(message));
+  });
 
   subscribers = [];
+
+  ctx.body = "ok";
+
 });
+
+/*
+// this minimalistik resolve is work, but not full, tests are not resolve
+
+ router.get('/subscribe', async (ctx, next) => {
+
+ const promise = new Promise((resolve, reject) => subscribers.push(resolve));
+
+ let message = await promise;
+
+ ctx.body = message;
+
+ });
+
+ router.post('/publish', async (ctx, next) => {
+
+ const message = ctx.request.body.message;
+
+ if (!message) ctx.throw(400);
+
+ subscribers.forEach(resolve => {resolve(message.toString())});
+
+ subscribers = [];
+ });*/
 
 app.use(router.routes());
 
