0d169add7fb24d7f59e0f16c1ec161bcdcd2ed97
Marat Shagiev <m_js@bk.ru>
2019-11-18 00:24:55 +0300

    start 6-module 2-task

--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
   ctx.body = {products: []};
 };
@@ -7,6 +9,10 @@ module.exports.productList = async function productList(ctx, next) {
 };
 
 module.exports.productById = async function productById(ctx, next) {
+  if (!mongoose.Types.ObjectId.isValid(ctx.params.id)) ctx.throw(400, 'id инвалид');
+
+  ctx.status = 404;
   ctx.body = {product: {}};
+
 };
 
