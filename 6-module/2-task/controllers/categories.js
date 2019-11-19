aed4228ff29dbc9b3d1d4f5c4e93415e004831fa
Marat Shagiev <m_js@bk.ru>
2019-11-19 19:19:12 +0300

    continue 6-module 2-task

--- 6-module/2-task/controllers/categories.js
+++ 6-module/2-task/controllers/categories.js
@@ -1,3 +1,7 @@
+const Category = require('../models/Category');
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const cats = await Category.find();
+  ctx.body = cats;
+  // ctx.body = {categories: []};
 };
