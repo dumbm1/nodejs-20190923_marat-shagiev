9db9be23c7800db574e83d7a4bbc6eb00b2d2577
Marat Shagiev <m_js@bk.ru>
2019-12-09 15:35:44 +0300

    Try to fix solution 6-module 2-task...

--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -2,28 +2,44 @@ const mongoose = require('mongoose');
 const Product = require('../models/Product');
 
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  const products = await Product.find({subcategory: ctx.request.body.id});
+  if (!mongoose.Types.ObjectId.isValid(ctx.request.body.id)) {
+    ctx.body = {'products': []};
+    ctx.throw(400, 'id инвалид-c');
+    return next();
+  }
+
+  const prodsBySubcatId = await Product.find({subcategory: ctx.request.body.id});
+
+  ctx.body = prodsBySubcatId;
+  return next();
+};
+
+module.exports.productList = function productList(ctx, next) {
+
+  const prodsBySubcatId = ctx.body;
+
+  if (!prodsBySubcatId[0]) {
+    ctx.throw(404, `Подкатегория с id ${ctx.request.body.id} отсутсвует`);
+    ctx.body = {'products': []};
+    return next();
+  }
 
   const resultProducts = [];
 
-  for (let i = 0; i < products.length; i++) {
-    const product = products[i];
+  for (let i = 0; i < prodsBySubcatId.length; i++) {
+    const product = prodsBySubcatId[i];
 
     resultProducts.push({
-      id: product._id,
-      title: product.title,
-      images: product.images,
-      category: product.category,
+      id         : product._id,
+      title      : product.title,
+      images     : product.images,
+      category   : product.category,
       subcategory: ctx.request.body.id,
-      price: product.price,
+      price      : product.price,
       description: product.description,
     });
   }
   ctx.body = {'products': resultProducts};
-};
-
-module.exports.productList = async function productList(ctx) {
-  ctx.body = {'products': []};
 };
 
 module.exports.productById = async function productById(ctx) {
@@ -34,12 +50,12 @@ module.exports.productById = async function productById(ctx) {
   if (!product) ctx.throw(404, `Товар с id ${ctx.params.id} отсутсвует`);
 
   const resultProduct = {
-    id: ctx.params.id,
-    title: product.title,
-    images: product.images,
-    category: product.category,
+    id         : ctx.params.id,
+    title      : product.title,
+    images     : product.images,
+    category   : product.category,
     subcategory: product.subcategory,
-    price: product.price,
+    price      : product.price,
     description: product.description,
   };
 
