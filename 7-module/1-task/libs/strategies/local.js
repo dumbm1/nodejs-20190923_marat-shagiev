3e2806e3b39541c7f4e8c6e3f92e8b7beb66f96d
Marat Shagiev <m_js@bk.ru>
2020-01-06 23:23:55 +0300

    Fix solution 7-module 1-task

--- 7-module/1-task/libs/strategies/local.js
+++ 7-module/1-task/libs/strategies/local.js
@@ -2,18 +2,17 @@ const LocalStrategy = require('passport-local').Strategy;
 const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
-    {session: false},
+  {session: false},
 
-    async function (email, password, done)  {
-      const user = await User.findOne({email});
+  async function (email, password, done) {
+    const user = await User.findOne({email});
 
-      if (!user) return done(null, false, 'Нет такого пользователя');
+    if (!user) return done(null, false, 'Нет такого пользователя');
 
-      const isValidPassword = await user.checkPassword(password);
+    const isValidPassword = await user.checkPassword(password);
 
-      if(!isValidPassword) return done(null, false, 'password incorrect');
+    if (!isValidPassword) return done(null, false, 'Неверный пароль');
 
-      done(null, user);
-    }
-
+    done(null, user);
+  }
 );
