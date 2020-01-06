0ea46081614ac2399ab0cc7cab926b72ae5bbf9f
Marat Shagiev <m_js@bk.ru>
2020-01-06 22:47:15 +0300

    Add solution 7-module 1-task: make local strategy in file libs/strategies/local.js

--- 7-module/1-task/libs/strategies/local.js
+++ 7-module/1-task/libs/strategies/local.js
@@ -3,7 +3,17 @@ const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
     {session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+
+    async function (email, password, done)  {
+      const user = await User.findOne({email});
+
+      if (!user) return done(null, false, 'Нет такого пользователя');
+
+      const isValidPassword = await user.checkPassword(password);
+
+      if(!isValidPassword) return done(null, false, 'password incorrect');
+
+      done(null, user);
     }
+
 );
