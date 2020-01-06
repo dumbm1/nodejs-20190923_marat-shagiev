d6c7d4c05a6f6f5d546eea74d97a04d2cdaeb414
Marat Shagiev <m_js@bk.ru>
2020-01-06 23:45:22 +0300

    Solution 7-module 1-task: fix passport usernameField

--- 7-module/1-task/libs/strategies/local.js
+++ 7-module/1-task/libs/strategies/local.js
@@ -2,7 +2,7 @@ const LocalStrategy = require('passport-local').Strategy;
 const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
-  {session: false},
+  {session: false, usernameField: 'email'},
 
   async function (email, password, done) {
     const user = await User.findOne({email});
