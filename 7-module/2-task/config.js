f4eb46f09476f8b5b164dd01b0f3989b20502f39
Marat Shagiev <m_js@bk.ru>
2020-01-09 22:44:26 +0300

    7-module 2-task: add to config.js fb, vk, gh ids and secret keys

--- 7-module/2-task/config.js
+++ 7-module/2-task/config.js
@@ -11,25 +11,25 @@ module.exports = {
   },
   providers: {
     github: {
-      app_id: process.env.GITHUB_APP_ID || 'github_app_id',
-      app_secret: process.env.GITHUB_APP_SECRET || 'github_app_secret',
+      app_id: process.env.GITHUB_APP_ID || 'df328f2e4e62701e2a46',
+      app_secret: process.env.GITHUB_APP_SECRET || '9c76c56d7b37bedacdee834b1b7ccac144cc5674',
       callback_uri: 'http://localhost:3000/oauth/github',
       options: {
         scope: ['user:email'],
       },
     },
     facebook: {
-      app_id: process.env.FACEBOOK_APP_ID || 'facebook_app_id',
-      app_secret: process.env.FACEBOOK_APP_SECRET || 'facebook_app_secret',
+      app_id: process.env.FACEBOOK_APP_ID || '453159062022493',
+      app_secret: process.env.FACEBOOK_APP_SECRET || '313e178a3e8e5d99f64557acc2364e6c',
       callback_uri: 'http://localhost:3000/oauth/facebook',
       options: {
         scope: ['email'],
       },
     },
     vkontakte: {
-      app_id: process.env.VKONTAKTE_APP_ID || 'vkontakte_app_id',
-      app_secret: process.env.VKONTAKTE_APP_SECRET || 'vkontakte_app_secret',
-      callback_uri: 'http://localhost:3000/oauth/vkontakte',
+      app_id: process.env.VKONTAKTE_APP_ID || '7273037',
+      app_secret: process.env.VKONTAKTE_APP_SECRET || 'odN0Dj4p3f1xLuZKT3vt',
+      callback_uri: 'http://localhost:3000/api/oauth/vkontakte',
       options: {
         scope: ['email'],
       },
